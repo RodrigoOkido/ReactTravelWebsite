3e96845dc34db9c516443b0ecc423fef54c09a27
RodrigoOkido <rodrigookido@gmail.com>
2020-10-12 20:09:45 -0300

    Updates

---
+++ precache-manifest.53b472a8afb6e8debcbd006b43457abc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3f64470a2d81c8efc19f8de49b03fa",
+    "url": "/ReactTravelWebsite/index.html"
+  },
+  {
+    "revision": "45a8b28c699c5d9276de",
+    "url": "/ReactTravelWebsite/static/css/main.478d02e6.chunk.css"
+  },
+  {
+    "revision": "d510e47f0dc0b371a0aa",
+    "url": "/ReactTravelWebsite/static/js/2.3c00a545.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactTravelWebsite/static/js/2.3c00a545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a8b28c699c5d9276de",
+    "url": "/ReactTravelWebsite/static/js/main.e816bba2.chunk.js"
+  },
+  {
+    "revision": "fd1c1c9f3ed1bccc44a0",
+    "url": "/ReactTravelWebsite/static/js/runtime-main.7a8940e8.js"
+  },
+  {
+    "revision": "d727edb01be9cf9caeba56df34102ba7",
+    "url": "/ReactTravelWebsite/static/media/amsterdam.d727edb0.jpg"
+  },
+  {
+    "revision": "ae8789a9f4c70fd3b1cfa34c66610c65",
+    "url": "/ReactTravelWebsite/static/media/beach.ae8789a9.jpg"
+  },
+  {
+    "revision": "d498915cab0504516a255dcfc62f6776",
+    "url": "/ReactTravelWebsite/static/media/california.d498915c.jpg"
+  },
+  {
+    "revision": "1d2e2d19305c3848260d68b0aa4a8ad9",
+    "url": "/ReactTravelWebsite/static/media/curitiba.1d2e2d19.jpg"
+  },
+  {
+    "revision": "2c19251e5d18372210453f4a8fd5cea7",
+    "url": "/ReactTravelWebsite/static/media/frankfurt.2c19251e.jpg"
+  },
+  {
+    "revision": "31ea38248d612b2541e6d1cdab328dd9",
+    "url": "/ReactTravelWebsite/static/media/hamburg.31ea3824.jpg"
+  },
+  {
+    "revision": "7c2ce7d647e1c31df41b0b9e504f65f4",
+    "url": "/ReactTravelWebsite/static/media/lisboa.7c2ce7d6.jpg"
+  },
+  {
+    "revision": "962f466eae6c7fa82d3b9943fb91f565",
+    "url": "/ReactTravelWebsite/static/media/london.962f466e.jpg"
+  },
+  {
+    "revision": "8ac4f7ea8bfc9e13c0ab1366786438b0",
+    "url": "/ReactTravelWebsite/static/media/miami.8ac4f7ea.jpg"
+  },
+  {
+    "revision": "416947fbc3443284862fc8151a8131b4",
+    "url": "/ReactTravelWebsite/static/media/montevideo.416947fb.jpg"
+  },
+  {
+    "revision": "e2c055c484475852368b4b8fba642119",
+    "url": "/ReactTravelWebsite/static/media/munich.e2c055c4.jpg"
+  },
+  {
+    "revision": "b22a0d73fb58c2dc2e64efa93295e7df",
+    "url": "/ReactTravelWebsite/static/media/newyork.b22a0d73.jpg"
+  },
+  {
+    "revision": "b00f04a73c2549d939b8bd331bb750d3",
+    "url": "/ReactTravelWebsite/static/media/osaka.b00f04a7.jpg"
+  },
+  {
+    "revision": "6798935fb418cbe37f394989d2264ba0",
+    "url": "/ReactTravelWebsite/static/media/paris.6798935f.jpg"
+  },
+  {
+    "revision": "0877b4269dec3f38a20af865ff54786c",
+    "url": "/ReactTravelWebsite/static/media/porto-alegre.0877b426.jpg"
+  },
+  {
+    "revision": "0dda62c6158d0247e38d5c38a4a41774",
+    "url": "/ReactTravelWebsite/static/media/rio-de-janeiro.0dda62c6.jpg"
+  },
+  {
+    "revision": "39cd124986b0ac2ebd30962d47972a9b",
+    "url": "/ReactTravelWebsite/static/media/saopaulo.39cd1249.jpg"
+  },
+  {
+    "revision": "f323834d108c97d019a21b09ada2df04",
+    "url": "/ReactTravelWebsite/static/media/seattle.f323834d.jpg"
+  },
+  {
+    "revision": "380d7b8dc709a58080d4d538b260461c",
+    "url": "/ReactTravelWebsite/static/media/seoul.380d7b8d.jpg"
+  },
+  {
+    "revision": "2e9b45f9c0b7415fa5d902118bcbba94",
+    "url": "/ReactTravelWebsite/static/media/tokyo.2e9b45f9.jpg"
+  },
+  {
+    "revision": "da1168c98c2198e5a9dfb02fce6fb812",
+    "url": "/ReactTravelWebsite/static/media/vancouver.da1168c9.jpg"
+  },
+  {
+    "revision": "77ec3ad61362c4ec79855c3d5cf77a25",
+    "url": "/ReactTravelWebsite/static/media/vegas.77ec3ad6.jpg"
+  }
+]);
